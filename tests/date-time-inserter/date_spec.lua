62d1ec1a384f80217225894f244acaeeea8f30dc
Anton Van Assche <vanasscheanton@gmail.com>
2024-12-14 17:56:19 +0100

    Add spec test for date module

---
+++ tests/date-time-inserter/date_spec.lua
@@ -0,0 +1,37 @@
+-- tests/date-time-inserter/date_spec.lua
+
+local M = require("date-time-inserter.date")
+local assert = require("luassert")
+local os = require("os")
+
+local mock_date = "2024-12-14"
+os.date = function()
+  return mock_date
+end
+
+describe("Date Format Tests", function()
+  it("should return the date in MM/DD/YYYY format", function()
+    local result = M.setup("MMDDYYYY", "/")
+    assert.are.same("12/14/2024", result)
+  end)
+
+  it("should return the date in DD/MM/YYYY format", function()
+    local result = M.setup("DDMMYYYY", "/")
+    assert.are.same("14/12/2024", result)
+  end)
+
+  it("should return the date in YYYY/MM/DD format", function()
+    local result = M.setup("YYYYMMDD", "/")
+    assert.are.same("2024/12/14", result)
+  end)
+
+  it("should work with a different separator", function()
+    local result = M.setup("MMDDYYYY", "-")
+    assert.are.same("12-14-2024", result)
+  end)
+
+  it("should handle formats with repeated characters", function()
+    local result = M.setup("MMMMYYYYDD", "-")
+    assert.are.same("12-2024-14", result)
+  end)
+end)
